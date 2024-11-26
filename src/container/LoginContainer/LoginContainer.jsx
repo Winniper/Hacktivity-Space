010ac4f2e2c5d93f54c483d0afddfe4905316782
Debaditya <debadityabarman12@gmail.com>
2024-11-26 21:59:00 +0530

    adding auth

--- src/container/LoginContainer/LoginContainer.jsx
+++ src/container/LoginContainer/LoginContainer.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react"
 import LoginView from "../../views/LoginView/LoginView"
+import { auth } from "../../lib/config/FirestoreConfig"
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 const LoginContainer = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const unsubsribe = onAuthStateChanged(auth, (user) => {
+      if (user){
+        navigate("/home")
+      }
+    })
+    return () => unsubsribe()
+  },[navigate])
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value)
+  }
+  
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async() => {
+    try {
+      await signInWithEmailAndPassword(auth, username, password)
+      navigate("/home")
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div>
-        <LoginView/>
+        <LoginView username={username} password={password} handleUsername={handleUsername} handlePassword={handlePassword} onClick={handleLogin}/>
     </div>
   )
 }
---
+++ src/lib/config/FirestoreConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSCat4huiKy6vT8ZV_3Av71NOG20k14AA",
+  authDomain: "hacktivity-space.firebaseapp.com",
+  projectId: "hacktivity-space",
+  storageBucket: "hacktivity-space.firebasestorage.app",
+  messagingSenderId: "1074291968621",
+  appId: "1:1074291968621:web:a3446ec1cb8adfc1f90d64",
+  measurementId: "G-083W374QXT"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
