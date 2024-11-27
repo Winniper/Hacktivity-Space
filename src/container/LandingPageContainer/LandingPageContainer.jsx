9265a3f78cad172305a657fa3d9ed3c3ec5277db
Surya Rathee <ratheesurya23@gmail.com>
2024-11-27 09:41:27 +0530

    yes

--- src/container/LandingPageContainer/LandingPageContainer.jsx
+++ src/container/LandingPageContainer/LandingPageContainer.jsx
@@ -1,4 +1,4 @@
-import TypewriterView from "../../views/TypeWriterView/TypewriterView"
+// import TypewriterView from "../../views/TypeWriterView/TypewriterView"
 import { useNavigate } from "react-router-dom"
 
 
@@ -9,7 +9,15 @@ const LandingPageContainer = () => {
     }
     
     return(
-        <TypewriterView onClick={handleClick}/>
+        <div className="h-screen flex items-center justify-center bg-[#000000] flex-col space-y-10">
+        <Particle/>
+      <span className="text-white text-3xl font-poppins tracking-tighter font-medium"><span className="text-[#3B82F6] text-3xl font-poppins">Hacktivity</span> Space</span>
+      <TextAnimation/>
+      <div className="flex space-x-6">
+        <CustomButton variant={"primary"} onClick={onClick}>Login</CustomButton>
+      </div>
+    </div>
+        // <TypewriterView onClick={handleClick}/>
     )
 }
 
